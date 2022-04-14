67e5bc9e0f5378814059a9688e5f895b1a4b8450
963994623 <963994623@qq.com>
2022-04-14 15:33:17 +0800

    feat:

---
+++ utils/utils.js
@@ -0,0 +1,50 @@
+const log4js = require("./utils/log4j");
+
+//状态码
+const Code = {
+  SUCCESS: 200,
+  PARAM_ERROR: 10001,
+  USER_ACCOUNT_ERROR: 20001,
+  USER_LOGIN_ERROR: 30001,
+  BUSINESS_ERROR: 40001,
+  AUTH_ERROR: 50001,
+};
+
+/**
+ * 分页结构封装
+ * @param {number} pageNum
+ * @param {number} pageSize
+ */
+function pager({ pageNum = 1, pageSize = 10 }) {
+  pageNum *= 1;
+  pageSize *= 1;
+  const skipIndex = (pageNum - 1) * pageSize;
+  return {
+    page: {
+      pageNum,
+      pageSize,
+    },
+    skipIndex,
+  };
+}
+
+function success(data = "", msg = "", code = Code.SUCCESS) {
+  log4js.debug(data);
+  return {
+    data,
+    msg,
+    code,
+  };
+}
+function fail(msg = "", code = Code.BUSINESS_ERROR) {
+  log4js.debug(data);
+  return {
+    code,
+    data,
+    msg,
+  };
+}
+
+module.exports = {
+  pager,
+};
